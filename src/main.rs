2cef6eecccdcfa67590e0b0c80bc3dfadfb63c3b
Michael Mezzina <michaelmez39@gmail.com>
2020-07-10 20:01:14 -0400

    Commit before merging with master

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use plotters::prelude::*;
-use std::io::prelude::*;
 use serde::Deserialize;
 use std::fs::{read_dir, File};
+use std::io::prelude::*;
 // Province_State,
 // Country_Region,Last_Update,Lat,Long_,Confirmed,Deaths,
 // Recovered,Active,FIPS,Incident_Rate,People_Tested,People_Hospitalized,
@@ -14,14 +14,14 @@ struct DailyReport {
     Deaths: Option<u64>,
     Recovered: Option<u64>,
     People_Tested: Option<u64>,
-    Hospitalization_Rate: f64
+    Hospitalization_Rate: f64,
 }
 
 #[derive(Debug)]
 enum AppError {
     ConfigurationNotFound,
     InvalidConfiguration,
-    InvalidData
+    InvalidDatapath,
 }
 
 type AppResult<T> = Result<T, AppError>;
@@ -34,16 +34,29 @@ impl Default for DailyReport {
             Deaths: Some(0),
             Recovered: Some(0),
             People_Tested: Some(0),
-            Hospitalization_Rate: 0.0
+            Hospitalization_Rate: 0.0,
         }
     }
 }
 
-#[derive(Deserialize)]
+impl DailyReport {
+    fn get_prop(&self, name: &str) -> Option<f64> {
+        match name.to_ascii_lowercase().as_str() {
+            "confirmed" => Some(self.Confirmed as f64),
+            "deaths" => self.Deaths.map(|x| x as f64),
+            "recovered" => self.Recovered.map(|x| x as f64),
+            "people_tested" => self.People_Tested.map(|x| x as f64),
+            "hospitalization_rate" => Some(self.Hospitalization_Rate),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Deserialize, Clone)]
 struct Config {
     state: String,
     statistic: String,
-    datapath: String
+    datapath: String,
 }
 
 impl std::cmp::PartialOrd for DailyReport {
@@ -53,48 +66,32 @@ impl std::cmp::PartialOrd for DailyReport {
 }
 
 fn load_configuration() -> AppResult<Config> {
-    let mut config_file = File::open("config.toml").map_err(|x| AppError::ConfigurationNotFound)?;
+    let mut config_file = File::open("config.toml").map_err(|_| AppError::ConfigurationNotFound)?;
     let mut raw_config = String::new();
     config_file.read_to_string(&mut raw_config).unwrap();
-    toml::from_str(&raw_config).map_err(|x| AppError::InvalidConfiguration)
+    toml::from_str(&raw_config).map_err(|_| AppError::InvalidConfiguration)
 }
-// 6, 7, 8
-// confirmed, deaths, recovered
-fn main() -> AppResult<()> {
-    let config = load_configuration();
-    
-    let data_path = config.datapath;
-    let mut confirmed: Vec<DailyReport> = Vec::new();
-    let reports = read_dir(data_path).expect("COVID_PATH invalid");
-    let mut num_reports = 0;
-    for file in reports {
-        if let Ok(d) = file {
-            num_reports += 1;
-            let data = File::open(d.path()).map_err(|x| AppError::InvalidConfiguration)?;
-            let mut rdr = csv::Reader::from_reader(data);
-            confirmed.push(
-                rdr.deserialize()
-                    .filter(|record: &Result<DailyReport, csv::Error>| {
-                        record.is_ok() && record.as_ref().unwrap().Province_State == config.state
-                    })
-                    .nth(0).unwrap_or(Ok(DailyReport::default())).unwrap()
-            )
-        }
-    }
-    confirmed.sort_by(|a,b| a.Confirmed.cmp(&b.Confirmed));
-    let confirmed: Vec<(i32, DailyReport)> = confirmed.into_iter().enumerate().map(|(a, b)| (a as i32, b)).collect();
-    println!("{:?}", confirmed);
+
+fn generate_plot(config: &Config, data: Vec<DailyReport>) {
     let root = BitMapBackend::new("output.png", (640, 480)).into_drawing_area();
     root.fill(&WHITE).unwrap();
     let root = root.margin(10, 10, 10, 10);
+
+    let max = data
+        .iter()
+        .map(|rep| rep.get_prop(&config.statistic).unwrap_or(0.0) as usize)
+        .max().unwrap_or(0) + 1;
     let mut chart = ChartBuilder::on(&root)
         // Set the caption of the chart
-        .caption("Florida Hospitiliaztion Rate", ("sans-serif", 40).into_font())
+        .caption(
+            format!("{}: {} vs DAYS ELAPSED", config.state, config.statistic.to_uppercase()),
+            ("sans-serif", 40).into_font(),
+        )
         // Set the size of the label region
         .x_label_area_size(20)
         .y_label_area_size(40)
         // Finally attach a coordinate on the drawing area and make a chart context
-        .build_ranged(0..num_reports, 0.0..20.0)
+        .build_ranged(0..data.len(), 0.0..max as f64)
         .unwrap();
 
     chart
@@ -105,11 +102,44 @@ fn main() -> AppResult<()> {
         .y_desc("log10[# People]")
         // We can also change the format of the label text
         .y_label_formatter(&|x| format!("{:.3}", x))
-        .draw().unwrap();
+        .draw()
+        .unwrap();
 
-    chart.draw_series(LineSeries::new(
-        confirmed.iter().map(|x| (x.0, (x.1).Hospitalization_Rate)),
-        &RED,
-    )).unwrap();
+    chart
+        .draw_series(LineSeries::new(
+            (0..data.len()).zip(
+                data.iter()
+                    .map(|rep| rep.get_prop(&config.statistic).unwrap_or(0.0)),
+            ),
+            &RED,
+        ))
+        .unwrap();
+}
+// 6, 7, 8
+// confirmed, deaths, recovered
+fn main() -> AppResult<()> {
+    let config = load_configuration()?;
+
+    let sources = read_dir(&config.datapath).map_err(|_| AppError::InvalidDatapath)?;
+    let mut reports: Vec<DailyReport> = Vec::new();
+    for file in sources {
+        if let Ok(d) = file {
+            let data = File::open(d.path()).map_err(|_| AppError::InvalidConfiguration)?;
+            let mut rdr = csv::Reader::from_reader(data);
+            reports.push(
+                rdr.deserialize()
+                    .filter(|record: &Result<DailyReport, csv::Error>| {
+                        record.is_ok() && record.as_ref().unwrap().Province_State == config.state
+                    })
+                    .nth(0)
+                    .unwrap_or(Ok(DailyReport::default()))
+                    .unwrap(),
+            )
+        }
+    }
+
+    reports.sort_by(|a, b| a.partial_cmp(&b).unwrap());
+
+    generate_plot(&config, reports);
     Ok(())
 }
